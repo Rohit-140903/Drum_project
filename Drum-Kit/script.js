814fd736433f898a35b692c79825ffdee3f8cbbe
Rohit-140903 <145894662+Rohit-140903@users.noreply.github.com>
2024-02-16 00:32:53 +0530

    Add files via upload

---
+++ Drum-Kit/script.js
@@ -0,0 +1,73 @@
+var numberOfDrumsButton = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumsButton; i++) {
+
+    document.querySelectorAll("button")[i].addEventListener("click", function () {  //  anonymus function call
+
+        // var audio = new Audio("sounds/tom-1.mp3");
+        // audio.play(); syntax for adding sound in js..
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+
+     });
+
+} // click by cursor
+
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key){
+
+    switch (key) {
+        case "w":
+            var audio1 = new Audio("sounds/tom-1.mp3")
+            audio1.play();
+            break;
+        case "a":
+            var audio2 = new Audio("sounds/tom-2.mp3")
+            audio2.play();
+            break;
+        case "s":
+            var audio3 = new Audio("sounds/tom-3.mp3")
+            audio3.play();
+            break;
+        case "d":
+            var audio4 = new Audio("sounds/tom-4.mp3")
+            audio4.play();
+            break;
+        case "j":
+            var audio5 = new Audio("sounds/snare.mp3")
+            audio5.play();
+            break;
+        case "k":
+            var audio6 = new Audio("sounds/crash.mp3")
+            audio6.play();
+            break;
+        case "l":
+            var audio7 = new Audio("sounds/kick-bass.mp3")
+            audio7.play();
+            break;
+        default:
+            break;
+    }
+
+} // key press by keyboard
+
+
+function buttonAnimation(currentKey){  // timeout function
+
+    var activeButton = document.querySelector("."+currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function (){ // Anonymus function
+
+        activeButton.classList.remove("pressed");
+    },100); // setTimeout is inbuilt function and it accepts one function and another one is time which is 1s or 1000 millisec then 0.1s will be 100ms.
+    // .pressed is a class type and it is used in css style sheet
+    
+
+}
